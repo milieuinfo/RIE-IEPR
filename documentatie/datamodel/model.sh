99134743bceb759888f4418ddc52031a3ad52e4b
gezever <gezever@skynet.be>
2026-01-19 14:42:30 +0100

    rdf2dot

--- documentatie/datamodel/model.sh
+++ documentatie/datamodel/model.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-rm /tmp/luchtwassers.nt
-  for i in  `find ../huidige_metingen/ | grep jsonld` ; do
-    riot ${i} >> /tmp/luchtwassers.nt
-done
-riot --formatted=turtle /tmp/luchtwassers.nt > /tmp/luchtwassers.ttl
 
-  sparql --results=TTL --data=/tmp/luchtwassers.ttl  --query model.rq  > model.ttl
-  rdf2dot  model.ttl | dot -Tpng > model.png
-  rdf2dot  model.ttl  > model.dot
+
+riot --formatted=turtle `find '../../src/main/resources/be/vlaanderen/omgeving/riepr/data/id' -type f` > /tmp/riepr.ttl
+
+sparql --results=TTL --data=/tmp/riepr.ttl  --query model.rq | sed -e 's/label/Label/g' > model.ttl
+rdf2dot  model.ttl | dot -Tpng > model.png
+rdf2dot  model.ttl  > model.dot
