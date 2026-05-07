bfab65af432cc71a32ebd05e47042f5d7b9e0426
gezever <gezever@skynet.be>
2026-05-07 15:41:37 +0200

    tweetalig

--- src/main/input/imjv-transformed/code/construct.sh
+++ src/main/input/imjv-transformed/code/construct.sh
@@ -2,10 +2,11 @@
 
 function set_virtuoso_env {
 	ssh ${1} "sudo cat /etc/*/*bootstrap | grep virtuoso" > /tmp/${1}
-	. /tmp/${1}
+
 }
 
 function query {
+  	. /tmp/${1}
 	curl -u ${virtuoso_rw_username}:${virtuoso_rw_password}\
 		--request POST "http://${1}:8080/sparql-auth?"\
 		--data 'format=text/turtle'\
@@ -17,4 +18,5 @@ function query {
 
 
 set_virtuoso_env virtuoso-imjv-pr-1.vm.cumuli.be
+
 query virtuoso-imjv-pr-1.vm.cumuli.be
