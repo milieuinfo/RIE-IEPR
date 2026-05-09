77fbaead52850ce83fefcc7ffbde38386821dff9
gezever <gezever@skynet.be>
2026-05-09 16:16:31 +0200

    imjv execution

---
+++ src/main/input/imjv-transformed/code/source/2025/systems.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+function set_virtuoso_env {
+	ssh ${1} "sudo cat /etc/*/*bootstrap | grep virtuoso" > /tmp/${1}
+
+}
+
+function query {
+  #let x=1
+  #for uri in `cat systems` ; do
+  #let x=27
+  #for uri in `cat observaties` ; do
+  let x=104
+  for uri in `cat zuiveringsapparaat` ; do
+    echo "construct { ?s ?p ?o .  ?x ?y ?z . }  where {VALUES ?s { <$uri>  }  VALUES ?z { <$uri>  } ?s ?p ?o .   optional{ ?x ?y ?z .}}" > query.rq
+  	. /tmp/${1}
+    curl -u ${virtuoso_rw_username}:${virtuoso_rw_password}\
+      --request POST "http://${1}:8080/sparql-auth?"\
+      --data 'format=text/turtle'\
+      --data-urlencode query@query.rq \
+      --output '/tmp/mjv.ttl'
+    riot '/tmp/mjv.ttl' > '/tmp/mjv.nt'
+    riot --formatted=turtle prefix.ttl '/tmp/mjv.nt' > $x.ttl
+    let x=x+1
+  done
+}
+
+set_virtuoso_env virtuoso-imjv-pr-1.vm.cumuli.be
+
+query virtuoso-imjv-pr-1.vm.cumuli.be
+
+
