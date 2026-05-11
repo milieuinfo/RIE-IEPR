17e6158f1969bfd0ef568909e83ac2fed712b16e
gezever <gezever@skynet.be>
2026-05-11 17:18:57 +0200

    exploitatie

--- src/main/input/imjv-transformed/code/construct_deployment.sh
+++ src/main/input/imjv-transformed/code/construct_deployment.sh
@@ -13,11 +13,25 @@ function query {
 		--data-urlencode query@construct_deployment.rq \
 		--output '/tmp/mjv.ttl'
 	riot '/tmp/mjv.ttl'  > '/tmp/mjv.nt'
-  riot --formatted=turtle source/prefix.ttl '/tmp/mjv.nt' > '../mjv_deployment.ttl'
 }
 
+function query2 {
+  	. /tmp/${1}
+	curl -u ${virtuoso_rw_username}:${virtuoso_rw_password}\
+		--request POST "http://${1}:8080/sparql-auth?"\
+		--data 'format=text/turtle'\
+		--data-urlencode query@construct_exploitant.rq \
+		--output '/tmp/mjv_exploitant.ttl'
+	riot '/tmp/mjv_exploitant.ttl' \
+		> '/tmp/mjv_exploitant.nt'
+	riot --formatted=turtle source/prefix.ttl '/tmp/mjv.nt' '/tmp/mjv_exploitant.nt' > '../mjv_deployment.ttl'
+}
 
 
 set_virtuoso_env virtuoso-imjv-pr-1.vm.cumuli.be
 
 query virtuoso-imjv-pr-1.vm.cumuli.be
+
+set_virtuoso_env virtuoso-cbb-pr-1.vm.cumuli.be
+
+query2 virtuoso-cbb-pr-1.vm.cumuli.be
