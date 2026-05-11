22d7273de8c827798d2d8bdd4c6f5749fc9ea61b
gezever <gezever@skynet.be>
2026-05-11 18:06:32 +0200

    volledige validatie

--- src/main/input/imjv-transformed/code/construct_deployment.sh
+++ src/main/input/imjv-transformed/code/construct_deployment.sh
@@ -5,6 +5,16 @@ function set_virtuoso_env {
 
 }
 
+function query_labels {
+	. /tmp/${1}
+	curl -u ${virtuoso_rw_username}:${virtuoso_rw_password}\
+		--request POST "http://${1}:8080/sparql-auth?"\
+		--data 'format=text/turtle'\
+		--data-urlencode query@construct_labels.rq \
+		--output '/tmp/mjv_labels.ttl'
+	riot '/tmp/mjv_labels.ttl' > '/tmp/mjv_labels.nt'
+}
+
 function query {
   	. /tmp/${1}
 	curl -u ${virtuoso_rw_username}:${virtuoso_rw_password}\
@@ -24,13 +34,18 @@ function query2 {
 		--output '/tmp/mjv_exploitant.ttl'
 	riot '/tmp/mjv_exploitant.ttl' \
 		> '/tmp/mjv_exploitant.nt'
-	riot --formatted=turtle source/prefix.ttl '/tmp/mjv.nt' '/tmp/mjv_exploitant.nt' > '../mjv_deployment.ttl'
+	riot --formatted=turtle source/prefix.ttl \
+		'/tmp/mjv_labels.nt' \
+		'/tmp/mjv.nt' \
+		'/tmp/mjv_exploitant.nt' \
+		> '../mjv_deployment.ttl'
 }
 
 
 set_virtuoso_env virtuoso-imjv-pr-1.vm.cumuli.be
 
-query virtuoso-imjv-pr-1.vm.cumuli.be
+query_labels virtuoso-imjv-pr-1.vm.cumuli.be
+query        virtuoso-imjv-pr-1.vm.cumuli.be
 
 set_virtuoso_env virtuoso-cbb-pr-1.vm.cumuli.be
 
