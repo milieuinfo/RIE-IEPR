631e8823b01fc338dd48c2a1db6810ccbcc5237c
gezever <gezever@skynet.be>
2026-05-15 15:31:06 +0200

    dump

--- src/main/input/imjv-transformed/dump/bin/sort.sh
+++ src/main/input/imjv-transformed/dump/bin/sort.sh
@@ -3,12 +3,18 @@
 riot --formatted=turtle ../*ttl > /tmp/all.ttl
 
 sparql --results=CSV --data=/tmp/all.ttl  --query select_types.rq  > ../types.csv
+
+type_list=$(grep -v '^\s*#' type_priority.txt | grep -v '^\s*$')
+
 let x=1
-for c in $(grep -v 'type' ../types.csv); do
+excl_uris=""
+
+for c in $type_list; do
   class=$(echo "$c" | tr -d '\r\n' )
   className=$(echo "$class" | sed 's;.*/;;g' | sed 's;#;:;g')
   mkdir -p ../sorted/$className
-  sed "s|___type___|$class|g" construct_sorts.rq > /tmp/construct_sorts.rq
+  sed "s|___type___|$class|g; s|___excluded_types___|$excl_uris|g" construct_sorts.rq > /tmp/construct_sorts.rq
   sparql --results=TTL --data=/tmp/all.ttl  --query /tmp/construct_sorts.rq > ../sorted/$className/$x.ttl
+  excl_uris="$excl_uris <$class>"
   let x=x+1
 done
