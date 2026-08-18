75076049f67a946945346e6e0cef14d07ed4a9b8
jenkins-systeemgebruiker <noreply-jenkins@omgeving.vlaanderen.be>
2026-08-18 11:37:43 +0000

    docs: deploy from jenkins-Publiek-RIE-IEPR-main-41

--- extra/mermaid-init.js
+++ extra/mermaid-init.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
   if (window.mermaid) {
-    mermaid.initialize({ startOnLoad: true, theme: 'default' });
+    mermaid.initialize({ 
+      startOnLoad: true, 
+      theme: 'forest',
+      themeVariables: {
+        primaryColor: '#007A87',
+        primaryTextColor: '#fff',
+        primaryBorderColor: '#005f6a',
+        lineColor: '#007A87',
+        secondaryColor: '#e6f4f5',
+        tertiaryColor: '#b2e0e3'
+      }
+    });
   }
 });
