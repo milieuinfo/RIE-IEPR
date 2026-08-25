5efd80a0eeb85ef00815361e88c1534dc547f434
jenkins-systeemgebruiker <noreply-jenkins@omgeving.vlaanderen.be>
2026-08-25 14:11:15 +0000

    chore: ODDToolkit gegenereerde artefacten herbouwd
    Cumulus-Skip-Ci: true

--- documentatie/datamodel/generated/sql/schema.sql
+++ documentatie/datamodel/generated/sql/schema.sql
@@ -1,6 +1,6 @@
 -- Auto-generated SQL schema from ODDToolkit
 -- Ontology: null
--- Generated: 2026-08-25T13:41:28.128962901Z[GMT]
+-- Generated: 2026-08-25T14:06:59.862775043Z[GMT]
 
 -- http://www.w3.org/ns/sosa/Procedure
 CREATE TYPE procedure AS ENUM (
